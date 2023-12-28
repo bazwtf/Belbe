86227fdd595210e8b34c21ac03d02b565bc4996a
Barry Mackie-Conlon <hello@baz.wtf>
2023-12-28 22:51:06 +0000

    continued work on basic functions to pull in life totals. still work in progress

--- popups/popup.js
+++ popups/popup.js
@@ -1,15 +1,50 @@
 const startObserving = document.getElementById("startObserving");
-startObserving.addEventListener("click",() => {    
+
+startObserving.addEventListener("click",() => {
+    // Get active browser tab
     chrome.tabs.query({active: true}, (tabs) => {
         const tab = tabs[0];
         if (tab) {
-            alert(tab.id)
+            execScript(tab);
         } else {
-            alert("There are no active tabs")
+            alert("There are no active tabs");
         }
     })
 })
 
-// TODO: Eventually looking to grab value from all inputs with label of "aria-label="Life Total"
-// Example HTML Object
-// <input class="focus:outline-none bg-transparent font-bold text-center select-auto text-white text-3xl" type="number" aria-label="Life Total" value="40" style="width: 59px;">
+/**
+ * Function executes a getLifeTotals() function on a web page,
+ * opened on specified tab
+ * @param tab - A tab to execute script on
+ */
+function execScript(tab) {
+    // Execute a function on a page of the current browser tab
+    // and process the result of execution
+    chrome.scripting.executeScript(
+        {
+            target:{tabId: tab.id, allFrames: true},
+            func:getLifeTotals
+        },
+        onResult
+    );
+}
+
+function getLifeTotals() {
+    // TODO - Get life totals from all players
+    // Return an array of the values
+
+    const lifeTotals = document.querySelectorAll('[aria-label="Life Total"]');
+    console.log("hello", lifeTotals);
+    return Array.from(lifeTotals);
+}
+
+function onResult(frames) {
+    // If script execution failed on the remote end 
+    // and could not return results
+    if (!frames || !frames.length) { 
+        alert("Could not retrieve life totals from specified page");
+        return;
+    }
+
+    console.log(frames);
+}
